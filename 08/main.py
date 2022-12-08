ac47864c9331ea32b6ed7099337f5ea681e22652
Lucia Munoz martinez <lmunoz-m@c3r2s1.42madrid.com>
2022-12-08 13:37:00 +0100

    day finished

--- 08/main.py
+++ 08/main.py
@@ -1,19 +1,14 @@
 
 	
 # Read the input.txt file.
-with open('input2.txt') as file:
+with open('input.txt') as file:
   	grid = [list(line.strip()) for line in file]
 
-#30373 00 01 02 03 04
-#25512 10 11 12 13 14
-#65332 20 21 22 23 24
-#33549 30 31 32 33 34
-#35390 40 41 42 43 44
-""" max_i = max(grid[i])
-  print('max_i = ', max_i) """
-
-print(grid)
 res = 0
+
+# Part 1
+
+visitedNumbers = []
 
 for i in range(len(grid)): # i = filas
     for j in range(len(grid[i])): # j = columnas
@@ -22,6 +17,13 @@ for i in range(len(grid)): # i = filas
         res += 1
       elif j == 0 or j == len(grid[i]) - 1:
         res += 1
+
+
+#make a bidimensional array of visited numbers with binary values
+for i in range(len(grid)):
+  visitedNumbers.append([])
+  for j in range(len(grid[i])):
+    visitedNumbers[i].append(0)
 
 
 max = 0
@@ -33,7 +35,9 @@ for j in range(1,len(grid)-1):
     #print(' i = ', i, ' j = ', j, 'valor = ', grid[i][j], 'max = ', max)
     if grid[i][j] > max:
       max = grid[i][j]
-      res += 1
+      if(visitedNumbers[i][j] == 0):
+        visitedNumbers[i][j] = 1
+        res += 1
       #print('cambio el max por = ', max)
 
 #para derecha
@@ -43,7 +47,9 @@ for i in range(1,len(grid)-1):
     #print(' i = ', i, ' j = ', j, 'valor = ', grid[i][j], 'max = ', max)
     if grid[i][j] > max:
       max = grid[i][j]
-      res += 1
+      if(visitedNumbers[i][j] == 0):
+        visitedNumbers[i][j] = 1
+        res += 1
       #print('cambio el max por = ', max)   
 
 #para izquierda
@@ -53,7 +59,9 @@ for i in range(1,len(grid)-1):
     #print(' i = ', i, ' j = ', j, 'valor = ', grid[i][j], 'max = ', max)
     if grid[i][j] > max:
       max = grid[i][j]
-      res += 1
+      if(visitedNumbers[i][j] == 0):
+        visitedNumbers[i][j] = 1
+        res += 1
       #print('cambio el max por = ', max) 
 
 #para arriba
@@ -63,11 +71,64 @@ for j in range(1,len(grid)-1):
     #print(' i = ', i, ' j = ', j, 'valor = ', grid[i][j], 'max = ', max)
     if grid[i][j] > max:
       max = grid[i][j]
-      res += 1
+      if(visitedNumbers[i][j] == 0):
+        visitedNumbers[i][j] = 1
+        res += 1
       #print('cambio el max por = ', max)
 
+print('star1: ', res)
 
+
+# Part 2
+#30373 00 01 02 03 04
+#25512 10 11 12 13 14
+#65332 20 21 22 23 24
+#33549 30 31 32 33 34
+#35390 40 41 42 43 44
+res = 0
+for i in range(1,len(grid)): # i = filas
+    for j in range(1,len(grid[i])): # j = columnas
+      act = 1
+      #abajo
+      k = 1
+      while(i + k < len(grid)):   
+        k += 1
+        if (grid[i+k-1][j] >= grid[i][j]):
+          break
+      k -= 1
+      act *= k
+      #arriba
+      k = 1;
+      while(i - k >= 0):
+          k += 1
+          if(grid[i - (k - 1)][j] >= grid[i][j]):
+              break;
+      k -= 1;
+      act *= k;
+      #derecha
+      k = 1;
+      while(j + k < len(grid[i])):
+        k += 1
+        if(grid[i][j + k - 1] >= grid[i][j]):
+            break;
+      k -= 1;
+      act *= k;
+      #izquierda
+      k = 1;
+      while(j - k >= 0):
+        k += 1
+        if(grid[i][j - (k - 1)] >= grid[i][j]):
+            break;
+      k -= 1;
+      act *= k;
+
+      if(act > res):
+        res = act
+      
+      
 
 print(res)
+
+
             
 
