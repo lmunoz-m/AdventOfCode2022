5039d239694340354cd486862f85e9490530f91d
lmunoz-m <luciamunoz057@gmail.com>
2022-12-09 12:09:15 +0100

    funciona para tablero estatico

--- 09/main.py
+++ 09/main.py
@@ -14,7 +14,7 @@ gridT = [
 ['.','.','.','.','.','.',], # 30 31 32 33 34 35
 ['.','.','.','.','.','.',]]
 
-archivo = open("input2.txt")
+archivo = open("input.txt")
 
 
 # create an empty list to store the instructions from the file
@@ -38,7 +38,9 @@ def findT(grid):
 	for i in range(len(grid)):
 		for j in range(len(grid[i])):
 			if grid[i][j] == 'H':
-				if i-1 >= 0 and grid[i-1][j] == 'T':
+				if grid[i][j] == 'T':
+					res = True
+				elif i-1 >= 0 and grid[i-1][j] == 'T':
 					res = True
 				elif i+1 < len(grid) and grid[i+1][j] == 'T':
 					res = True
@@ -54,7 +56,9 @@ def findT(grid):
 					res = True
 				elif i+1 < len(grid) and j+1 < len(grid[i]) and grid[i+1][j+1] == 'T':
 					res = True
-	
+
+
+
 	if (res == False):
 		#find the pos where 'T' is and change it to '.'
 		for i in range(len(grid)):
@@ -64,10 +68,10 @@ def findT(grid):
 
 	return res
 
-	
 # apply the instructions to the matrix and remove then once is done
 print(instructions)
 k = 1
+r = 0
 while instructions:
 	direcc, moves = instructions[0]
 	instructions.pop(0)
@@ -78,38 +82,78 @@ while instructions:
 			#print('paso2')
 			if (grid [i][j] == 'H' and isChange == False ):
 				print('estoy pasando por ', k, ' vez')
-				if direcc == 'U' and i-moves >= 0:
-					grid[i-moves][j] = 'H'
+				if direcc == 'U':
+					q = 0
+					while q < moves:
+						if i-q-1 >= 0:
+							if grid[i-q-1][j] == 'T':
+								grid[i-q-1][j] = 'H'
+								grid[i-q][j] = '.'
+							else:
+								grid[i-q-1][j] = 'H'
+								grid[i-q][j] = '.'
+								if findT(grid) == False:
+									grid[i-q][j] = 'T'
+									gridT[i-q][j] = '#'
+									r += 1
+							isChange = True
+						q += 1
 
-					grid[i][j] = '.'
-					if findT(grid) == False:
-						grid[i-moves+1][j] = 'T'
 						
 					isChange = True
-				elif direcc == 'D' and i+moves < len(grid):
-					grid[i+moves][j] = 'H'
 
-					grid[i][j] = '.'
-					if findT(grid) == False:
-						grid[i+moves-1][j] = 'T'
-						
-					isChange = True
-				elif direcc == 'L' and j-moves >= 0:
-					grid[i][j-moves] = 'H'
-					grid[i][j] = '.'
+				elif direcc == 'D':
+					q = 0
+					while q < moves:
+						if i+q+1 < len(grid):
+							if grid[i+q+1][j] == 'T':
+								grid[i+q+1][j] = 'H'
+								grid[i+q][j] = '.'
+							else:
+								grid[i+q+1][j] = 'H'
+								grid[i+q][j] = '.'
+								if findT(grid) == False:
+									grid[i+q][j] = 'T'
+									gridT[i+q][j] = '#'
+									r += 1
+							isChange = True
+						q += 1
 
-					if findT(grid) == False:
-						grid[i][j-moves+1] = 'T'
-						
-					isChange = True
-				elif direcc == 'R' and j+moves < len(grid[i]):
-					grid[i][j+moves] = 'H'
-					grid[i][j] = '.'
+				elif direcc == 'L':
+					q = 0
+					while q < moves:
+						if j-q-1 >= 0:
+							if grid[i][j-q-1] == 'T':
+								grid[i][j-q-1] = 'H'
+								grid[i][j-q] = '.'
+							else:
+								grid[i][j-q-1] = 'H'
+								grid[i][j-q] = '.'
+								if findT(grid) == False:
+									grid[i][j-q] = 'T'
+									gridT[i][j-q] = '#'
+									r += 1
+							isChange = True
+						q += 1
 
-					if findT(grid) == False:
-						grid[i][j+moves-1] = 'T'
+				elif direcc == 'R' :
+						q = 0
+						while q < moves:
+							if j+q+1 < len(grid[i]):
+							# si en algun momento 'H' se pone en donde esta 'T' entonces se cambia a '.'
+								if grid[i][j+q+1] == 'T':
+									grid[i][j+q+1] = 'H'
+									grid[i][j+q] = '.'
+								else:
+									grid[i][j+q+1] = 'H'
+									grid[i][j+q] = '.'
+									if findT(grid) == False:
+										grid[i][j+q] = 'T'
+										gridT[i][j+q] = '#'
+										r += 1
+								isChange = True
+							q += 1
 						
-					isChange = True
 				print('dirección: ', direcc, 'movimientos: ', moves)
 				print(grid[0],'\n')
 				print(grid[1],'\n')
@@ -117,7 +161,13 @@ while instructions:
 				print(grid[3],'\n')
 				print(grid[4],'\n')
 				k += 1
-				print(instructions)
+				print('tablero posiciones de T\n')
+				print(gridT[0],'\n')
+				print(gridT[1],'\n')
+				print(gridT[2],'\n')
+				print(gridT[3],'\n')
+				print(gridT[4],'\n') 
+				#print(instructions)
 				break
 			
 		
@@ -131,10 +181,20 @@ print(grid[3],'\n')
 print(grid[4],'\n') 
 
 print('tablero posiciones de T\n')
+#gridT[4][0] = 's'
 print(gridT[0],'\n')
 print(gridT[1],'\n')
 print(gridT[2],'\n')
 print(gridT[3],'\n')
 print(gridT[4],'\n') 
+
+#contar cuantas # hay en gridT
+count = 0
+for i in range(len(gridT)):
+	for j in range(len(gridT[i])):
+		if gridT[i][j] == '#':
+			count += 1
+
+print('cantidad de T: ', r)
 
 archivo.close()
